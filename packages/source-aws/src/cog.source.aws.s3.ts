309c750aa369b77c13bb0b1ae54cfe2e8c5ae60a
Blayne Chard <blayne@chard.com>
2020-10-29 12:01:48 +1300

    feat(source-aws): remove aws-sdk typings
    AWS SDK update's its typings quite regularly and will often cause conflicts for people using a different version of aws-sdk in their project.
    
    As the fetcher needs a very simple API `bucket, key, range => Buffer` Using a s3Like interface removes the reliance on the aws-sdk
    
    BREAKING CHANGE: a default s3 object needs to be used when using the S3 Source, this can be set with `
    CogSourceAwsS3.DefaultS3 = new S3();`

--- packages/source-aws/src/cog.source.aws.s3.ts
+++ packages/source-aws/src/cog.source.aws.s3.ts
@@ -1,5 +1,4 @@
 import { CogLogger, CogSource, CogSourceChunked, CogTiff } from '@cogeotiff/core';
-import * as S3 from 'aws-sdk/clients/s3';
 
 class CompositeError extends Error {
     reason: Error;
@@ -9,8 +8,17 @@ class CompositeError extends Error {
     }
 }
 
+export interface S3LikeResponse {
+    promise(): Promise<{ Body?: Buffer | unknown }>;
+}
+export interface S3Like {
+    getObject(req: { Bucket: string; Key: string; Range: string }): S3LikeResponse;
+}
+
 export class CogSourceAwsS3 extends CogSourceChunked {
     type = 'aws-s3';
+
+    static DefaultS3: S3Like;
 
     // HTTP gets are slow, get a larger amount
     chunkSize: number = 64 * 1024;
@@ -20,20 +28,20 @@ export class CogSourceAwsS3 extends CogSourceChunked {
 
     bucket: string;
     key: string;
-    s3: S3;
+    remote: S3Like;
 
-    constructor(bucket: string, key: string, s3: S3 = new S3()) {
+    constructor(bucket: string, key: string, remote: S3Like) {
         super();
         this.bucket = bucket;
         this.key = key;
-        this.s3 = s3;
+        this.remote = remote;
     }
 
-    get uri() {
+    get uri(): string {
         return this.name;
     }
 
-    get name() {
+    get name(): string {
         return `s3://${this.bucket}/${this.key}`;
     }
 
@@ -72,35 +80,35 @@ export class CogSourceAwsS3 extends CogSourceChunked {
      *
      * @param uri URI to parse
      */
-    static createFromUri(uri: string): CogSourceAwsS3 | null {
+    static createFromUri(uri: string, remote?: S3Like): CogSourceAwsS3 | null {
         const res = CogSourceAwsS3.parse(uri);
-        if (res == null) {
-            return null;
-        }
-        return new CogSourceAwsS3(res.bucket, res.key);
+        if (res == null) return null;
+        return new CogSourceAwsS3(res.bucket, res.key, remote ?? CogSourceAwsS3.DefaultS3);
     }
 
     /**
      * Load a COG from a AWS S3 Bucket
      * @param uri URI to load `s3://foo/baz.tiff`
+     * @param remote Optional S3 Object to use for the requests
      */
-    static async create(uri: string): Promise<CogTiff>;
+    static async create(uri: string, remote?: S3Like): Promise<CogTiff>;
     /**
      * Load a COG from a AWS S3 Bucket
      *
      * @param bucket AWS S3 Bucket name
      * @param key Path to COG inside of bucket
+     * @param remote Optional S3 Object to use for the requests
      */
-    static async create(bucket: string, key: string): Promise<CogTiff>;
-    static async create(sA: string, key?: string): Promise<CogTiff> {
-        if (key == null) {
-            const source = CogSourceAwsS3.createFromUri(sA);
+    static async create(bucket: string, key: string, remote?: S3Like): Promise<CogTiff>;
+    static async create(sA: string, key?: string | S3Like, remote?: S3Like): Promise<CogTiff> {
+        if (typeof key != 'string') {
+            const source = CogSourceAwsS3.createFromUri(sA, key);
             if (source == null) {
                 throw new Error(`Invalid URI ${sA}`);
             }
             return new CogTiff(source).init();
         }
-        return new CogTiff(new CogSourceAwsS3(sA, key)).init();
+        return new CogTiff(new CogSourceAwsS3(sA, key, remote ?? CogSourceAwsS3.DefaultS3)).init();
     }
 
     protected async loadChunks(firstChunk: number, lastChunk: number, logger: CogLogger | null): Promise<ArrayBuffer> {
@@ -117,16 +125,13 @@ export class CogSourceAwsS3 extends CogSourceChunked {
             },
             'S3Get',
         );
-
         try {
-            const res = await this.s3
-                .getObject({
-                    Bucket: this.bucket,
-                    Key: this.key,
-                    Range: fetchRange,
-                })
+            const resp = await this.remote
+                .getObject({ Bucket: this.bucket, Key: this.key, Range: fetchRange })
                 .promise();
-            return (res.Body as Buffer).buffer;
+            if (!Buffer.isBuffer(resp.Body)) throw new Error('Failed to fetch object, Body is not a buffer');
+            const buffer = resp.Body;
+            return buffer.buffer.slice(buffer.byteOffset, buffer.byteOffset + buffer.byteLength);
         } catch (error) {
             logger?.error({ error, source: this.name, firstChunk, lastChunk, fetchRange }, 'FailedToFetch');
             throw new CompositeError(`Failed to fetch ${this.name} ${fetchRange}`, error);
