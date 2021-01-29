55c505aa6f86e1776ccac74f8d314290505549e2
Blayne Chard <blayne@chard.com>
2021-01-29 20:17:38 +1300

    refactor: dead code removal

--- packages/chunk/src/source.ts
+++
@@ -1,8 +0,0 @@
-// import { LogType } from './log';
-
-// // export interface Source {
-// //     isLittleEndian: boolean;
-// //     chunkSize: number;
-// //     chunks: Map<number, DataView>;
-// //     fetchBytes(offset: number, count: number, log?: LogType): Promise<ArrayBuffer>;
-// // }
