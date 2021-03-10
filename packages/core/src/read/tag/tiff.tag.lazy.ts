0cb8e5074d6753809c00def18dd6d900923b9085
Blayne Chard <blayne@chard.com>
2021-03-10 18:37:39 +1300

    fix(core): actually fetch bytes when needed by a lazy tag loader

--- packages/core/src/read/tag/tiff.tag.lazy.ts
+++ packages/core/src/read/tag/tiff.tag.lazy.ts
@@ -9,7 +9,7 @@ export class CogTiffTagLazy<T> extends CogTiffTagBase<T> {
     }
 
     async fetch(l: LogType): Promise<T> {
-        if (this.tiff.source.hasBytes(this.valuePointer, this.dataLength)) {
+        if (this.tiff.source.hasBytes(this.valuePointer, this.dataLength) === false) {
             await this.tiff.source.loadBytes(this.valuePointer, this.dataLength, l);
         }
         return this.readValue();
