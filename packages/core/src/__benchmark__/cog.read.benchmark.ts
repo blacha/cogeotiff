5935c10dae793d8961e71c17f4a1c2326dbc55f4
Blayne Chard <blayne@chard.com>
2021-09-11 14:14:48 +1200

    ci: add continous benchmarks (#734)

---
+++ packages/core/src/__benchmark__/cog.read.benchmark.ts
@@ -0,0 +1,16 @@
+import { CogTiff } from '../cog.tiff';
+import { SourceFile } from '@chunkd/source-file';
+
+/** Read a tile from every image inside of a tiff 300 tiles read */
+async function main(): Promise<void> {
+    for (let i = 0; i < 50; i++) {
+        const tiff = new CogTiff(new SourceFile(process.argv[process.argv.length - 1]));
+        await tiff.init();
+
+        // 6 images
+        for (const img of tiff.images) await img.getTile(0, 0);
+        await tiff.close();
+    }
+}
+
+main();
