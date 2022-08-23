49af9716f0400643be2085a933d2db2b8a988226
Blayne Chard <blayne@chard.com>
2022-08-23 16:57:26 +1200

    feat(core): import LERC, LZMA and JpegXL compression types

--- packages/core/src/const/tiff.mime.ts
+++ packages/core/src/const/tiff.mime.ts
@@ -5,10 +5,13 @@ export enum TiffMimeType {
     NONE = 'application/octet-stream',
     JPEG = 'image/jpeg',
     JP2 = 'image/jp2',
+    JPEGXL = 'image/jpegxl',
     WEBP = 'image/webp',
     ZSTD = 'application/zstd',
     LZW = 'application/lzw',
     DEFLATE = 'application/deflate',
+    LZERC = 'application/lerc',
+    LZMA = 'application/x-lzma',
 }
 
 export const TiffCompression: { [key: number]: TiffMimeType } = {
@@ -17,7 +20,10 @@ export const TiffCompression: { [key: number]: TiffMimeType } = {
     6: TiffMimeType.JPEG,
     7: TiffMimeType.JPEG,
     8: TiffMimeType.DEFLATE,
+    34887: TiffMimeType.LZERC,
+    34925: TiffMimeType.LZMA,
     34712: TiffMimeType.JP2,
     50000: TiffMimeType.ZSTD,
     50001: TiffMimeType.WEBP,
+    50002: TiffMimeType.JPEGXL,
 };
