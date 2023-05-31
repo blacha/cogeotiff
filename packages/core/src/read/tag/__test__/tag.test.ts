0bd0f798a9211263f0d39a22b98b9b23e7d0a1aa
Blayne Chard <blayne@chard.com>
2023-05-31 22:10:51 +1200

    build: switch to node:test

--- packages/core/src/read/tag/__test__/tag.test.ts
+++ packages/core/src/read/tag/__test__/tag.test.ts
@@ -1,27 +1,28 @@
 import { TestFileChunkSource } from '@chunkd/core/build/__test__/chunk.source.fake.js';
-import o from 'ospec';
+import { describe, beforeEach, it } from "node:test";
+import assert from "node:assert";
 import * as path from 'path';
 import { fileURLToPath } from 'url';
 import { CogTiff } from '../../../cog.tiff.js';
 import { CogTiffTagLazy } from '../tiff.tag.lazy.js';
 
-o.spec('TiffTag', () => {
+describe('TiffTag', () => {
     const dirName = fileURLToPath(import.meta.url);
     const cogSourceFile = new TestFileChunkSource(path.join(dirName, '../../../../../data/rgba8_tiled.tiff'));
 
-    o.beforeEach(() => {
+    beforeEach(() => {
         (cogSourceFile.chunks as Map<unknown, unknown>).clear();
     });
 
-    o('should load lazy tags', async () => {
+    it('should load lazy tags', async () => {
         const tiff = new CogTiff(cogSourceFile);
 
         await cogSourceFile.loadBytes(3680, 8);
         const lazy = new CogTiffTagLazy(339, tiff, 3680);
-        o(lazy.value).equals(null);
+        assert.equal(lazy.value, null);
 
         const res = await lazy.fetch();
-        o(res).deepEquals([18761, 43, 8, 0]);
-        o(lazy.value).deepEquals(res);
+        assert.deepEqual(res, [18761, 43, 8, 0]);
+        assert.deepEqual(lazy.value, res);
     });
 });
