c488bf6292068c70054d68fd7e40515e50028b2e
Blayne Chard <blayne@chard.com>
2023-07-20 15:27:19 +1200

    test: add readme example as a file to test compilation

---
+++ packages/core/src/__test__/example.ts
@@ -0,0 +1,27 @@
+import { SourceHttp } from '@chunkd/source-http';
+import { CogTiff } from '../index.js';
+
+async function main(): Promise<void> {
+  const source = new SourceHttp('https://example.com/cog.tif');
+  const tiff = await CogTiff.create(source);
+
+  /** Load a specific tile from a specific image */
+  const tile = await tiff.images[5].getTile(2, 2);
+  tile.bytes; // Raw image buffer or null if tile doesn't exist
+
+  /** Load the 5th image in the Tiff */
+  const img = tiff.images[5];
+  if (img.isTiled()) {
+    /** Load tile x:10 y:10 */
+    const tile = await img.getTile(10, 10);
+    tile.mimeType; // image/jpeg
+    tile.bytes; // Raw image buffer
+  }
+
+  /** Get the origin point of the tiff */
+  img.origin;
+  /** Bounding box of the tiff */
+  img.bbox;
+}
+
+main();
