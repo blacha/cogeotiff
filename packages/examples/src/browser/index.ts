62ae5569c520ce90ff1de0ce4d65769ecfa596b2
Blayne Chard <blayne@chard.com>
2023-07-20 15:26:01 +1200

    fix: improve example showing caching and chunking of requests

--- packages/examples/src/browser/index.ts
+++ packages/examples/src/browser/index.ts
@@ -1,19 +1,29 @@
 import { SourceHttp } from '@chunkd/source-http';
+import { SourceRequest, SourceMiddleware, SourceCallback, SourceView } from '@chunkd/source';
+import { SourceCache, SourceChunk } from '@chunkd/middleware';
 import { CogTiff } from '@cogeotiff/core';
 import { loadSingleTile } from './example.single.tile';
 
-// Trace all fetch requests
-SourceHttp.fetch = async (input, init): Promise<Response> => {
-  const startTime = performance.now();
-  const res = await fetch(input, init);
-  const duration = performance.now() - startTime;
-  console.log(`Fetch: ${input} status: ${res.status} duration: ${Number(duration.toFixed(4))}ms`);
-  return res;
+// Cache all requests to cogs
+const cache = new SourceCache({ size: 16 * 1024 * 1024 }); // 16MB Cache
+const chunk = new SourceChunk({ size: 16 * 1024 }); // Chunk requests into 16KB fetches
+const fetchLog: SourceMiddleware = {
+  name: 'fetch:log',
+  fetch(req: SourceRequest, next: SourceCallback) {
+    this.fetches.push(req);
+    this.bytesRead += req.length ?? 0;
+    console.log('Tiff:fetch', { href: req.source.url.href, offset: req.offset, length: req.length });
+    return next(req);
+  },
 };
 
 document.addEventListener('DOMContentLoaded', async () => {
-  const tiffSource = new SourceHttp('https://blayne.chard.com/world.webp.google.cog.tiff');
-  const tiff = await new CogTiff(tiffSource);
+  const tiffSource = new SourceView(new SourceHttp('https://blayne.chard.com/world.webp.google.cog.tiff'), [
+    chunk,
+    cache,
+    fetchLog,
+  ]);
+  const tiff = await CogTiff.create(tiffSource);
 
   const mainEl = document.createElement('div');
   console.log('Loaded: ', tiff.source.url, '\nImages:', tiff.images.length);
