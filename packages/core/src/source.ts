af870fa0dddc6ec2b17b8de609462ad344c1730d
Blayne Chard <blayne@chard.com>
2023-08-23 23:13:40 +1200

    docs: improve docs for Source

--- packages/core/src/source.ts
+++ packages/core/src/source.ts
@@ -1,11 +1,13 @@
 export interface Source {
   /** Where the source is located */
   url: URL;
-  /** Optional metadata about the source including the size which */
+
+  /** Optional metadata about the source including the size in bytes of the file */
   metadata?: {
     /** Number of bytes in the file if known */
     size?: number;
   };
+
   /** Fetch bytes from a source */
   fetch(offset: number, length?: number): Promise<ArrayBuffer>;
 
