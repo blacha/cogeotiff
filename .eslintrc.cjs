734fccb9f9635ecb73e924c68fc1f6e04aa122b6
Blayne Chard <blayne@chard.com>
2026-02-18 08:46:45 +1300

    build: swap to npm with trusted publishing (#1403)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,4 +9,8 @@ tsRules.rules['@typescript-eslint/require-await'] = 'off';
 // node23 needs "import type" when importing types
 tsRules.rules['@typescript-eslint/consistent-type-imports'] = 'error';
 
+// Disable no floating promises in tests until https://github.com/nodejs/node/issues/51292 is solved
+const testOverrides = cfg.overrides.find((ovr) => ovr.files.find((f) => f.includes('.test.ts')));
+testOverrides.rules['@typescript-eslint/no-floating-promises'] = 'off';
+
 module.exports = cfg;
